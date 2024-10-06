36fa0cf7a2ab8034de0df05c5c3854ab7f892dd6
Oskar Rasienski <oskar.rasienski@gmail.com>
2024-10-06 13:51:56 +0200

    The table display done

--- web/viewer/src/components/Table.js
+++ web/viewer/src/components/Table.js
@@ -15,7 +15,7 @@ const Table = ({updateState}) => {
                 const killed_filter = document.getElementById("killed").value;
 
                 const response = await fetch(`http://127.0.0.1:5000/get_db?pilot=${pilot_filter}&rio=${rio_filter}&modex=${modex_filter}&weapon_type=${weapon_type_filter}&weapon=${weapon_filter}&killed=${killed_filter}`);
-                const rdata = await response.json();
+                const rdata = await response.json()
                 console.log(rdata);
                 setData(rdata);
             } catch (err) {
@@ -29,7 +29,36 @@ const Table = ({updateState}) => {
 
     return (
         <div className="App-Table">
-
+            <table border="1" className="Db-Table">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Event</th>
+                        <th>Pilot</th>
+                        <th>Rio</th>
+                        <th>Weapon</th>
+                        <th>Target</th>
+                        <th>Range</th>
+                        <th>Speed</th>
+                        <th>Killed?</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {data.map((entry, index) => (
+                    <tr key={index}>
+                    <td>{entry.date}</td>
+                    <td>{entry.event}</td>
+                    <td>{entry.pilot_name}</td>
+                    <td>{entry.rio_name}</td>
+                    <td>{entry.weapon ? entry.weapon : ""}</td>
+                    <td>{entry.target ? entry.target : ""}</td>
+                    <td>{entry.range ? entry.range : ""}</td>
+                    <td>{entry.speed ? entry.speed : ""}</td>
+                    <td>{entry.destroyed ? "True" : "False"}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     )
 }
