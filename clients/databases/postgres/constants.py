6ea00c599c7113b2993e7c80ef75ad29ba513300
Hanz727 <oskar.rasienski@gmail.com>
2025-07-06 14:44:29 +0200

    made postgres query have forced order

--- clients/databases/postgres/constants.py
+++ clients/databases/postgres/constants.py
@@ -1,4 +1,12 @@
-DB_FETCH_QUERY = "SELECT * FROM public.debriefs ORDER BY \"ID\" ASC"
+DB_FETCH_QUERY = '''
+SELECT 
+    "DATE", "FL_NAME", "SQUADRON", "RIO_NAME", "PLT_NAME", "TAIL_NUMBER", 
+    "WEAPON_TYPE", "WEAPON", "TARGET", "TARGET_ALT", "OWN_ALT", "SPEED", 
+    "RANGE", "HIT", "DESTROYED", "QTY", "MSN_NR", "MSN_NAME", "EVENT", "NOTES", "ID"
+FROM public.debriefs
+ORDER BY "ID" ASC;
+'''
+
 DB_INSERT_QUERY = '''
             INSERT INTO debriefs (
         	"DATE", "FL_NAME", "SQUADRON", "RIO_NAME", "PLT_NAME", "TAIL_NUMBER", "WEAPON_TYPE", "WEAPON", "TARGET", 
