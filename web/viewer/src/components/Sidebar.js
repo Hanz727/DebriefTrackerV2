e89c1e93fbaaf1d2c2022b9514e46a7c1c61c735
Hanz727 <oskar.rasienski@gmail.com>
2025-07-30 15:29:20 +0200

    viewer button

--- web/viewer/src/components/Sidebar.js
+++ web/viewer/src/components/Sidebar.js
@@ -6,6 +6,7 @@ const Sidebar = () => {
         <div className="sidebar">
             <a href="/" className="nav-link">HOME</a>
             <a href="/reports" className="nav-link">AP STRIKE REPORTS</a>
+            <a href="/dmpi_db" className="nav-link">DMPI DATABASE</a>
             <a href="#" className="nav-link">DATA VIEWER</a>
             <a href="/tacview" className="nav-link">TACVIEW</a>
             <a href="/tracks" className="nav-link">REPLAYS</a>
